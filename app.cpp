f1f4d5e7b6440a3814cbaf3422c1c83f87de92e9
Dmitry Buggenhout <dmitry.buggenhout@gmail.com>
2023-01-10 03:52:00 +0100

    end 05 p2

--- app.cpp
+++ app.cpp
@@ -1,7 +1,9 @@
 #include "app.hpp"
 #include "pipeline.hpp"
 #include <GLFW/glfw3.h>
+#include <array>
 #include <cmath>
+#include <cstdint>
 #include <memory>
 #include <stdexcept>
 #include <vulkan/vulkan_core.h>
@@ -9,19 +11,22 @@
 namespace vkEngine {
 
 App::App() {
-    createPipelineLayout();
-    createPipeline();
-    createCommandBuffers();
+  createPipelineLayout();
+  createPipeline();
+  createCommandBuffers();
 }
 
 App::~App() {
-    vkDestroyPipelineLayout(vkEngineDevice.device(), pipelineLayout, nullptr);
+  vkDestroyPipelineLayout(vkEngineDevice.device(), pipelineLayout, nullptr);
 }
 
 void App::run() {
   while (!window.shouldClose()) {
     glfwPollEvents();
+    drawFrame();
   }
+
+  vkDeviceWaitIdle(vkEngineDevice.device());
 }
 
 void App::createPipelineLayout() {
@@ -47,6 +52,65 @@ void App::createPipeline() {
                                         "shaders/frag.spv", pipelineConfig);
 }
 
-void App::createCommandBuffers() {};
-void App::drawFrame() {};
+void App::createCommandBuffers() {
+  commandBuffers.resize(vkEngineSwapChain.imageCount());
+
+  VkCommandBufferAllocateInfo allocInfo{};
+  allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+  allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+  allocInfo.commandPool = vkEngineDevice.getCommandPool();
+  allocInfo.commandBufferCount = static_cast<uint32_t>(commandBuffers.size());
+
+  if (vkAllocateCommandBuffers(vkEngineDevice.device(), &allocInfo, commandBuffers.data()) !=
+      VK_SUCCESS) {
+    throw std::runtime_error("failed to allocate command buffers!");
+  }
+
+  for (int i = 0; i < commandBuffers.size(); i++) {
+    VkCommandBufferBeginInfo beginInfo{};
+    beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+
+    if (vkBeginCommandBuffer(commandBuffers[i], &beginInfo) != VK_SUCCESS) {
+      throw std::runtime_error("failed to begin recording command buffer!");
+    }
+
+    VkRenderPassBeginInfo renderPassInfo{};
+    renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+    renderPassInfo.renderPass = vkEngineSwapChain.getRenderPass();
+    renderPassInfo.framebuffer = vkEngineSwapChain.getFrameBuffer(i);
+
+    renderPassInfo.renderArea.offset = {0, 0};
+    renderPassInfo.renderArea.extent = vkEngineSwapChain.getSwapChainExtent();
+
+    std::array<VkClearValue, 2> clearValues{};
+    clearValues[0].color = {0.1f, 0.1f, 0.1f, 1.0f};
+    clearValues[1].depthStencil = {1.0f, 0};
+    renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
+    renderPassInfo.pClearValues = clearValues.data();
+
+    vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+
+    pipeline->bind(commandBuffers[i]);
+    vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
+
+    vkCmdEndRenderPass(commandBuffers[i]);
+    if (vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS) {
+      throw std::runtime_error("failed to record command buffer!");
+    }
+  }
+}
+
+void App::drawFrame(){
+    uint32_t imageIndex;
+    auto result = vkEngineSwapChain.acquireNextImage(&imageIndex);
+
+    if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR ) {
+        throw std::runtime_error("failed to acquire swap chain image");
+    }
+
+    result = vkEngineSwapChain.submitCommandBuffers(&commandBuffers[imageIndex], &imageIndex);
+    if (result != VK_SUCCESS) {
+        throw std::runtime_error("failed to present swapchain image");
+    }
+}
 } // namespace vkEngine
